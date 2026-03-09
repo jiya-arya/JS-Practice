ed4319df0ec601b97d4524f3b9c178a19b7ed33b
Jiya <jiyaarya1205@gmail.com>
2026-03-09 22:06:32 +0530

    day 9 : creating vowels count

---
+++ day 9/script.js
@@ -0,0 +1,15 @@
+let btn = document.getElementById('countVowels');
+
+btn.addEventListener('click', function () {
+    console.log("btn clicked");
+
+    let word = document.getElementById('wordVowel').value;
+    console.log(word);
+
+    let display = document.getElementById('getCount');
+
+    let vowels = ['a','e','i','o','u']
+    console.log(vowels);
+    
+
+});
