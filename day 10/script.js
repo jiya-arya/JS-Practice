95013c6bb936bf1aa96da68ac8777361c6e5dc3e
Jiya <jiyaarya1205@gmail.com>
2026-03-13 18:12:57 +0530

    `Added event listeners and functionality for character and word counting, palindrome checking, and updated code with comments and whitespace`

--- day 10/script.js
+++ day 10/script.js
@@ -1,30 +1,69 @@
-let countChar = document.getElementById("countChar");
-let countWord = document.getElementById("countWords");
-let countBoth = document.getElementById("countBoth");
-let wordInput = document.getElementById("wordInput");
-let charCount = document.getElementById("charCount");
-let wordCount = document.getElementById("wordCount");
+// let countChar = document.getElementById("countChar");
+// let countWord = document.getElementById("countWords");
+// let countBoth = document.getElementById("countBoth");
+// let wordInput = document.getElementById("wordInput");
+// let charCount = document.getElementById("charCount");
+// let wordCount = document.getElementById("wordCount");
 
 // console.log(wordInput.value.length());
 
-countChar.addEventListener("click", function () {
-    console.log("btn count character clicked");
-    console.log(wordInput.value.length);
+// countChar.addEventListener("click", function () {
+//     console.log("btn count character clicked");
+//     console.log(wordInput.value.length);
 
-    let charLength = wordInput.value.length;
+//     let charLength = wordInput.value.length;
 
-    charCount.innerHTML = "Character Count: " + charLength;
-});
-countWord.addEventListener("click", function () {
-    console.log("btn count word clicked");
-    let wordLength = wordInput.value.trim().split(/\s+/).filter(Boolean).length;
-    wordCount.innerHTML = "Word Count: " + wordLength;
-});
-countBoth.addEventListener("click", function () {
-    console.log("btn count both clicked");
-     
-    let charLength = wordInput.value.length;
-    let wordLength = wordInput.value.trim().split(/\s+/).filter(Boolean).length;
-    charCount.innerHTML = "Character Count: " + charLength;
-    wordCount.innerHTML = "Word Count: " + wordLength;
+//     charCount.innerHTML = "Character Count: " + charLength;
+// });
+// countWord.addEventListener("click", function () {
+//     console.log("btn count word clicked");
+//     let wordLength = wordInput.value.trim().split(/\s+/).filter(Boolean).length;
+//     wordCount.innerHTML = "Word Count: " + wordLength;
+// });
+// countBoth.addEventListener("click", function () {
+//     console.log("btn count both clicked");
+
+//     let charLength = wordInput.value.length;
+//     let wordLength = wordInput.value.trim().split(/\s+/).filter(Boolean).length;
+//     charCount.innerHTML = "Character Count: " + charLength;
+//     wordCount.innerHTML = "Word Count: " + wordLength;
+// });
+
+// is palindrome 
+let checkPalindrome = document.getElementById("checkPalindrome");
+
+checkPalindrome.addEventListener("click", function () {
+
+    let result = document.getElementById("result");
+    let word = document.getElementById("InputWord");
+    console.log(word.value);
+
+    isPalin = isPalindrome(word.value);
+    console.log(isPalin);
+
+    if (isPalin) {
+        result.innerHTML = "The word is a palindrome";
+    } else {
+        result.innerHTML = "The word is not a palindrome";
+    }
+
+
+    function isPalindrome(word) {
+        let wordArray = word.split("");
+        console.log(wordArray);
+        let reverseWord = word.split("").reverse();
+        console.log(reverseWord);
+        console.log(wordArray);
+
+        isPalin = false;
+
+        for (let i = 0; i < wordArray.length; i++) {
+            console.log(wordArray[i], reverseWord[i]);
+            if (wordArray[i] === reverseWord[i]) {
+                isPalin = true;
+            }
+
+        }
+        return isPalin;
+    }
 });
