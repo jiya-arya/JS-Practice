3ea7cb585b64bc7945d6fa6775e89debe79e3a68
Jiya <jiyaarya1205@gmail.com>
2026-03-25 17:46:10 +0530

    modified second smallest

--- day 15/script.js
+++ day 15/script.js
@@ -1,20 +1,21 @@
-"//find second smallest number in the array"
+// Function to find the second smallest number in an array
 
 function secondSmallest(arr) {
-    let s = arr[0]
-    let ss = arr[1]
+    let s = Infinity;
+    let ss = Infinity;
 
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] < s) {
-            ss = s
-            s = arr[i]
-        }
-        else if (arr[i] < ss && arr[i] != s) {
-            ss = arr[i]
+            ss = s;
+            s = arr[i];
+        } 
+        else if (arr[i] < ss && arr[i] !== s) {
+            ss = arr[i];
         }
     }
-    return ss;
-}
-let result = secondSmallest([5, 2, 9, 1, 5, 6])
-console.log(result)
 
+    return ss === Infinity ? "No second smallest" : ss;
+}
+
+let result = secondSmallest([5, 2, 9, 1, 5, 6]);
+console.log(result);
