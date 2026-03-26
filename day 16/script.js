6206e26382ae231a42011a405e19826fdd29ba4c
Jiya <jiyaarya1205@gmail.com>
2026-03-26 13:21:24 +0530

    day 16 : project anagarams

---
+++ day 16/script.js
@@ -0,0 +1,17 @@
+//this is day 16 of JS practice
+
+// Problem:
+// You are given an array of strings.
+// 👉 You have to group words that are anagrams of each other.
+
+// Input:
+// ["eat", "tea", "tan", "ate", "nat", "bat"]
+
+// Output:
+// [
+//   ["eat", "tea", "ate"],
+//   ["tan", "nat"],
+//   ["bat"]
+// ]
+
+// Solution:
